40469791c2e2348a72571ca694d9f2dc4ccecc29
Pscaaghmn <92750286+Pscaaghmn@users.noreply.github.com>
2022-07-06 11:08:11 +0100

    Update ModifyEquipmentGUI.java

--- src/ModifyEquipmentGUI.java
+++ src/ModifyEquipmentGUI.java
@@ -3,6 +3,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ModifyEquipmentGUI extends JPanel implements ActionListener {
+    
+    private final Equipment item;
+    
+    public ModifyEquipmentGUI(Equipment item){
+        this.item = item;
+        
+        //TODO: + GUI
+    }
+    
     @Override
     public void actionPerformed(ActionEvent e) {
 
